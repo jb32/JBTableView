7ecb66f306c1ea1df87586bb6492e2aad09338ae
jb32 <2062964913@qq.com>
2015-05-17 16:41:02 +0800

    Initial Commit

---
+++ JBTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JBTableView
+//
+//  Created by 薛靖博 on 15/5/17.
+//  Copyright (c) 2015年 xjb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JBTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JBTableView
+//
+//  Created by 薛靖博 on 15/5/17.
+//  Copyright (c) 2015年 xjb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
